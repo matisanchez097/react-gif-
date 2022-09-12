5895103467e4e0189832c77e3ffa90a00da320df
Matias Sanchez <matisanchez097@gmail.com>
2022-09-12 18:24:11 -0300

    primer comit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = ({ target }) => {
+        setInputValue(target.value);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const newCategory = inputValue.trim()
+        if (newCategory.length <= 1) return;
+        //setCategories( categories => [...categories , inputValue]);
+        onNewCategory(newCategory);
+        setInputValue('');
+    }
+
+  return (
+    <>
+    <form onSubmit= { onSubmit }>
+      <input
+        type= 'text'
+        placeholder="Buscar Gifs"
+        value = {inputValue}
+        onChange= {onInputChange}
+        />
+    </form>
+    </>
+  )
+}
+
+
